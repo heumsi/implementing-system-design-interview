a37230dc333f41f19ff1fe7ebc74656bef899a6b
heumsi <heumsi@naver.com>
2022-12-02 00:51:04 +0900

    feat: add maximum number of requests that can be processed per second logic

--- 04-design-a-rate-limiter/src/main.py
+++ 04-design-a-rate-limiter/src/main.py
@@ -1,6 +1,8 @@
 import logging
 import socket
-from concurrent.futures import ThreadPoolExecutor
+from time import sleep
+import threading
+
 
 LISTEN_HOST = '0.0.0.0'
 LISTEN_PORT = 8000
@@ -9,7 +11,7 @@ FORWARD_PORT = 8080
 BUF_SIZE = 1024
 LOG_LEVEL = "DEBUG"
 LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-MAX_WORKERS = 10
+MAX_REQUESTS_PER_SECOND = 2
 
 logger = logging.getLogger()
 logger.setLevel(LOG_LEVEL)
@@ -17,35 +19,64 @@ stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(logging.Formatter(LOG_FORMAT))
 logger.addHandler(stream_handler)
 
+available = MAX_REQUESTS_PER_SECOND  # this will be reset periodically in set_available()
+
 
 def process(connection_socket, client_address):
-    with socket.socket() as forward_socket:
-        data = connection_socket.recv(BUF_SIZE)
-        decoded_data = data.decode()
-        logger.debug(f"got data from client {client_address[0]}:{client_address[1]}: {decoded_data}")
+    try:
+        with socket.socket() as forward_socket:
+            data = connection_socket.recv(BUF_SIZE)
+            decoded_data = data.decode()
+            logger.debug(f"got data from client {client_address[0]}:{client_address[1]}: {decoded_data}")
+            logger.debug(f"start to connect forward server {FORWARD_HOST}:{FORWARD_PORT}")
+            forward_socket.connect((FORWARD_HOST, FORWARD_PORT))
+            logger.debug(f"send data to forward server {FORWARD_HOST}:{FORWARD_PORT}")
+            forward_socket.send(data)
+            data = forward_socket.recv(BUF_SIZE)
+            decoded_data = data.decode()
+            logger.debug(f"got data from forward server {FORWARD_HOST}:{FORWARD_PORT}: {decoded_data}")
+        logger.debug(f"send data to client {client_address[0]}{client_address[1]}")
+        connection_socket.send(data)
+    finally:
+        connection_socket.close()
 
-        logger.debug(f"start to connect forward server {FORWARD_HOST}:{FORWARD_PORT}")
-        forward_socket.connect((FORWARD_HOST, FORWARD_PORT))
-        logger.debug(f"send data to forward server {FORWARD_HOST}:{FORWARD_PORT}")
-        forward_socket.send(data)
-        data = forward_socket.recv(BUF_SIZE)
-        decoded_data = data.decode()
-        logger.debug(f"got data from forward server {FORWARD_HOST}:{FORWARD_PORT}: {decoded_data}")
-    logger.debug(f"send data to client {client_address[0]}:{client_address[1]}")
-    connection_socket.send(data)
+
+def set_available():
+    while True:
+        global available
+        available = MAX_REQUESTS_PER_SECOND
+        sleep(5)
 
 
 with socket.socket() as server_socket:
+    set_available_thread = threading.Thread(target=set_available)
+    set_available_thread.start()
     server_socket.bind((LISTEN_HOST, LISTEN_PORT))
     server_socket.listen()
     logger.info(f"start listening on {LISTEN_HOST}:{LISTEN_PORT}")
-
-    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-        while True:
-            try:
-                connection_socket, client_address = server_socket.accept()
-                executor.submit(process, connection_socket, client_address)
-            except KeyboardInterrupt:
-                print("gracefully exit ...")
-                break
-        print("good bye!")
+    while True:
+        try:
+            connection_socket, client_address = server_socket.accept()
+            if available > 0:
+                available -= 1
+                process_thread = threading.Thread(target=process, args=(connection_socket, client_address))
+                process_thread.start()
+            else:
+                try:
+                    content = "Please retry after minutes"
+                    header = '\n'.join([
+                        "HTTP/1.1 429 Too many requests",
+                        "\n".join(f"{k}: {v}" for k, v in {
+                            'Content-Type': 'text/plan; encoding=utf8',
+                            'Content-Length': len(content),
+                            'Connection': 'close',
+                        }.items())
+                    ])
+                    data = "\n\n".join([header, content])
+                    connection_socket.send(data.encode('utf-8'))
+                finally:
+                    connection_socket.close()
+        except KeyboardInterrupt:
+            print("gracefully exit ...")
+            break
+print("good bye!")
