a71b7df879b34f92fb34e4353fa8bb4508bfed21
heumsi <heumsi@naver.com>
2022-12-02 00:54:19 +0900

    style(src/main.py): format codes

--- 04-design-a-rate-limiter/src/main.py
+++ 04-design-a-rate-limiter/src/main.py
@@ -1,10 +1,9 @@
 import logging
 import socket
-from time import sleep
 import threading
+from time import sleep
 
-
-LISTEN_HOST = '0.0.0.0'
+LISTEN_HOST = "0.0.0.0"
 LISTEN_PORT = 8000
 FORWARD_HOST = "127.0.0.1"
 FORWARD_PORT = 8080
@@ -19,7 +18,8 @@ stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(logging.Formatter(LOG_FORMAT))
 logger.addHandler(stream_handler)
 
-available = MAX_REQUESTS_PER_SECOND  # this will be reset periodically in set_available()
+# this will be reset periodically in set_available()
+available = MAX_REQUESTS_PER_SECOND
 
 
 def process(connection_socket, client_address):
@@ -27,14 +27,20 @@ def process(connection_socket, client_address):
         with socket.socket() as forward_socket:
             data = connection_socket.recv(BUF_SIZE)
             decoded_data = data.decode()
-            logger.debug(f"got data from client {client_address[0]}:{client_address[1]}: {decoded_data}")
-            logger.debug(f"start to connect forward server {FORWARD_HOST}:{FORWARD_PORT}")
+            logger.debug(
+                f"got data from client {client_address[0]}:{client_address[1]}: {decoded_data}"
+            )
+            logger.debug(
+                f"start to connect forward server {FORWARD_HOST}:{FORWARD_PORT}"
+            )
             forward_socket.connect((FORWARD_HOST, FORWARD_PORT))
             logger.debug(f"send data to forward server {FORWARD_HOST}:{FORWARD_PORT}")
             forward_socket.send(data)
             data = forward_socket.recv(BUF_SIZE)
             decoded_data = data.decode()
-            logger.debug(f"got data from forward server {FORWARD_HOST}:{FORWARD_PORT}: {decoded_data}")
+            logger.debug(
+                f"got data from forward server {FORWARD_HOST}:{FORWARD_PORT}: {decoded_data}"
+            )
         logger.debug(f"send data to client {client_address[0]}{client_address[1]}")
         connection_socket.send(data)
     finally:
@@ -59,21 +65,28 @@ with socket.socket() as server_socket:
             connection_socket, client_address = server_socket.accept()
             if available > 0:
                 available -= 1
-                process_thread = threading.Thread(target=process, args=(connection_socket, client_address))
+                process_thread = threading.Thread(
+                    target=process, args=(connection_socket, client_address)
+                )
                 process_thread.start()
             else:
                 try:
                     content = "Please retry after minutes"
-                    header = '\n'.join([
-                        "HTTP/1.1 429 Too many requests",
-                        "\n".join(f"{k}: {v}" for k, v in {
-                            'Content-Type': 'text/plan; encoding=utf8',
-                            'Content-Length': len(content),
-                            'Connection': 'close',
-                        }.items())
-                    ])
+                    header = "\n".join(
+                        [
+                            "HTTP/1.1 429 Too many requests",
+                            "\n".join(
+                                f"{k}: {v}"
+                                for k, v in {
+                                    "Content-Type": "text/plan; encoding=utf8",
+                                    "Content-Length": len(content),
+                                    "Connection": "close",
+                                }.items()
+                            ),
+                        ]
+                    )
                     data = "\n\n".join([header, content])
-                    connection_socket.send(data.encode('utf-8'))
+                    connection_socket.send(data.encode("utf-8"))
                 finally:
                     connection_socket.close()
         except KeyboardInterrupt:
