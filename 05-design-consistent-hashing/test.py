52dd474140a64f2f11b948aca8a8c2c79d19b8c7
heumsi <heumsi@naver.com>
2022-12-19 22:31:08 +0900

    test(05-design-consistent-hashing): add test_get_node_of_key_should_divide_the_keys_evenly()

--- 05-design-consistent-hashing/test.py
+++ 05-design-consistent-hashing/test.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from statistics import mean
 
 from consistent_hash import ConsistentHash, Node
@@ -24,6 +25,36 @@ def test_get_node_of_key_successfully():
 
         # then
         assert node.id in ["1", "2", "3"]
+
+
+def test_get_node_of_key_should_divide_the_keys_evenly():
+    # given
+    nodes = [
+        Node(id="1"),
+        Node(id="2"),
+        Node(id="3"),
+    ]
+    consistent_hash = ConsistentHash(nodes=nodes)
+    faker = Faker()
+    node_ids = []
+
+    # when
+    n_data_per_node = 100
+    n_data = n_data_per_node * len(nodes)
+    for _ in range(n_data):
+        key = faker.email()
+        node = consistent_hash.get_node_of_key(key)
+        node_ids.append(node.id)
+
+    # then
+    counter = Counter(node_ids)
+    error_rate = 0.05
+    lower_bound = n_data_per_node - int(n_data_per_node * error_rate)
+    upper_bound = n_data_per_node + int(n_data_per_node * error_rate)
+    expected_values = list(range(lower_bound, upper_bound))
+    assert counter["1"] in expected_values
+    assert counter["2"] in expected_values
+    assert counter["3"] in expected_values
 
 
 def test_content_hash_do_successfully():
